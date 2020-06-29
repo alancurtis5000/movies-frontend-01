0d7e9c3f17a5229e7befe4674c9c38e49970051a
alancurtis5000 <alancurtis5000+accounts@gmail.com>
2020-06-29 12:59:40 -0600

    added landing page.

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -5,13 +5,15 @@ import Header from './components/Header/Header';
 
 import APage01 from './pages/APage01/APage01';
 import APage02 from './pages/APage02/APage02';
+import Landing from './pages/Landing/Landing';
 
 const AppRouter = () =>{
   return(
     <BrowserRouter>
       <Header />
       <Switch>
-        <Route exact path="/" component={APage01}/>
+        <Route exact path="/" component={Landing}/>
+        <Route path="/page1" component={APage01}/>
         <Route path="/page2" component={APage02}/>
       </Switch>
     </BrowserRouter>
---
+++ src/pages/Landing/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Landing = () =>{
+  return(
+    <div className="Landing">
+      Landing
+      <div className="adds">
+        <div className="carosel">carosel</div>
+        <div className="add1">add1</div>
+        <div className="add2">add2</div>
+      </div>
+      <div className="movieSection">movieSection</div>
+    </div>
+  )
+}
+
+export default Landing;
